a64dfb11c47091133073acc52def87ab4f7a689d
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-11-12 18:33:19 -0500

    Add files via upload

---
+++ RUN_MAFFT.sh
@@ -0,0 +1,11 @@
+#Set up to run Protein MSA during this codon aware process.
+
+#mafft --auto FLTHIV1ENV2017DNA.fasta_protein.fas > FLTHIV1ENV2017DNA.fasta_protein.fas.msa
+mafft --auto FLTHIV1POL2017DNA.fasta_protein.fas > FLTHIV1POL2017DNA.fasta_protein.fas.msa
+mafft --auto FLTHIV1GAG2017DNA.fasta_protein.fas > FLTHIV1GAG2017DNA.fasta_protein.fas.msa
+mafft --auto FLTHIV1NEF2017DNA.fasta_protein.fas > FLTHIV1NEF2017DNA.fasta_protein.fas.msa
+mafft --auto FLTHIV1REV2017DNA.fasta_protein.fas > FLTHIV1REV2017DNA.fasta_protein.fas.msa
+mafft --auto FLTHIV1TAT2017DNA.fasta_protein.fas > FLTHIV1TAT2017DNA.fasta_protein.fas.msa
+mafft --auto FLTHIV1VIF2017DNA.fasta_protein.fas > FLTHIV1VIF2017DNA.fasta_protein.fas.msa
+mafft --auto FLTHIV1VPR2017DNA.fasta_protein.fas > FLTHIV1VPR2017DNA.fasta_protein.fas.msa
+mafft --auto FLTHIV1VPU2017DNA.fasta_protein.fas > FLTHIV1VPU2017DNA.fasta_protein.fas.msa
---
+++ RUN_PREMSA.sh
@@ -0,0 +1,10 @@
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1POL2017DNA.fasta
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1ENV2017DNA.fasta
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1GAG2017DNA.fasta
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1NEF2017DNA.fasta
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1REV2017DNA.fasta
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1TAT2017DNA.fasta
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1VIF2017DNA.fasta
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1VPR2017DNA.fasta
+hyphy pre-msa.bf /Users/alex/Documents/HIVDB-2017-DNA/HIV1-2017-Filtered/FLTHIV1VPU2017DNA.fasta
+
