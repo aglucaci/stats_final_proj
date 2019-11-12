37cf1364e67217d5e90a076bcc7ae366783006e8
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-11-12 18:55:54 -0500

    Add files via upload

---
+++ RUN_SEAVIEW.sh
@@ -0,0 +1,13 @@
+#Set up to run Seaview to build a NJ tree.
+
+#./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1ENV2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1ENV2017.msa
+./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1POL2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1POL2017.msa
+./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1GAG2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1GAG2017.msa
+./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1NEF2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1NEF2017.msa
+./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1REV2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1REV2017.msa
+./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1TAT2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1TAT2017.msa
+./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1VIF2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1VIF2017.msa
+./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1VPR2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1VPR2017.msa
+./seaview.app/Contents/MacOS/seaview -build_tree -distance observed -NJ -o /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1VPU2017.msa_NJ.nwk /Users/alex/Documents/hyphy-analyses/codon-msa/FLTHIV1VPU2017.msa
+
+
