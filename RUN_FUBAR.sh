9b151ba16b98dccfa418dc10ccca8c8e26608478
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-11-13 13:50:02 -0500

    Add files via upload

---
+++ RUN_FUBAR.sh
@@ -0,0 +1,21 @@
+#RUN FUBAR ON OUR HIV LANL DATA
+#NOTE: RUN THESE COMMANDS FROM THE HYPHY DIRECTORY
+
+#1
+#./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1GAG2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FLTHIV1GAG2017.msa_NJ.nwk
+#2
+./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1NEF2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FLTHIV1NEF2017.msa_NJ.nwk
+#3
+./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1POL2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FLTHIV1POL2017.msa_NJ.nwk
+#4
+./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1REV2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FLTHIV1REV2017.msa_NJ.nwk
+#5
+./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1VIF2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FLTHIV1VIF2017.msa.msa_NJ.nwk
+#6
+./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1VPR2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FLTHIV1VPR2017.msa_NJ.nwk
+#7
+./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1VPU2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FFLTHIV1VPU2017.msa_NJ.nwk
+#8
+./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1ENV2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FLTHIV1ENV2017.msa_NJ.nwk
+#9
+./hyphy /Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf --code Universal --alignment /Users/alex/Documents/stats_final_proj/Data/FLTHIV1TAT2017.msa --tree /Users/alex/Documents/stats_final_proj/Data/FLTHIV1TAT2017.msa_NJ.nwk
