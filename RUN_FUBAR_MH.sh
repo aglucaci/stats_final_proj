43a47da45928393e21d85d4488032409e2fa4775
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2019-11-18 10:27:52 -0500

    Add files via upload

---
+++ RUN_FUBAR_MH.sh
@@ -0,0 +1,24 @@
+#RUN FUBAR ON HIV LANL
+#NOTE: RUN FROM HYPHY DIRECTORY
+
+BATCHFILE="/Users/alex/hyphy/res/TemplateBatchFiles/SelectionAnalyses/FUBAR.bf"
+HYPHY="./hyphy"
+DATADIR="/Users/alex/Documents/stats_final_proj/Data"
+
+echo Starting to run FUBAR.. Initialize.
+
+#1
+
+for file in $DATADIR/*.msa; do
+  #echo "file=$file"
+  #echo $file
+  #echo checking output file $DATADIR"/"$file".MEME.json"
+  
+  if [[ ! -e $file".FUBAR_MH.json" ]]
+  then
+      echo Running FUBAR on $file
+      #echo hyphy $BATCHFILE --code Universal --alignment $file --tree $file"_NJ.nwk"
+      
+      $HYPHY $BATCHFILE --code Universal --method Metropolis-Hastings --alignment $file --tree $file"_NJ.nwk" --output $file".FUBAR_MH.json" --cache $file".FUBAR_MH.cache"
+  fi
+done
